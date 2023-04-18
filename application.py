fe5c7e5c78c3000c870b8f776a44712ff4e43f8a
LANDEROWSKI <110938474+LANDEROWSKI@users.noreply.github.com>
2023-04-18 01:02:08 -0600

    Login, Register y Buscar funcional

--- application.py
+++ application.py
@@ -1,14 +1,14 @@
 import os
 import requests
 
-from flask import Flask, session, render_template, redirect, request, jsonify, url_for
-#from helpers import login_required
+from flask import Flask, session, render_template, redirect, request, jsonify, url_for, flash
+# from helpers import login_required
 from flask_session import Session
 from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-from sesion import login_required           
+from sesion import login_required
 
 app = Flask(__name__)
 
@@ -46,7 +46,7 @@ def login():
         print(password)
 
         rows = db.execute(
-            text("SELECT * FROM users WHERE username=:username"), {'username':user}).fetchall()
+            text("SELECT * FROM users WHERE username=:username"), {'username': user}).fetchall()
         print(len(rows))
 
         if len(rows) != 1 or not check_password_hash(rows[0][2], password):
@@ -83,17 +83,138 @@ def registrar():
         if len(checkUser) != 0:
             return ("Usuario no disponible"), 400
 
-        db.execute(text('INSERT INTO users(username,password) values(:username, :password)') , {'username':usuario, 'password':hash_pass})
+        db.execute(text('INSERT INTO users(username,password) values(:username, :password)'), {
+                   'username': usuario, 'password': hash_pass})
         db.commit()
 
-        return redirect("/")
+        return render_template("login.html")
 
     else:
         return render_template("registro.html")
-
 
 
 @app.route('/logout')
 def logout():
     session.clear()
     return redirect(url_for('index'))
+
+
+@app.route("/buscar", methods=["GET", "POST"])
+@login_required
+def buscar():
+
+    if request.method == 'POST':
+        search = request.form.get('buscar')
+
+        # Este metodo 'title' vuelve todas las letras de cada palbra en mayuscula
+        search = search.title()
+
+        libros = db.execute(text(f"""SELECT * FROM book WHERE title LIKE '%{search}%' OR 
+        author LIKE '%{search}%' OR isbn LIKE '%{search}%'"""))
+
+        if libros.rowcount == 0:
+            return render_template('resultados.html', existe=False)
+
+        libros = libros.fetchall()
+
+        # print(libros)
+
+        return render_template('resultados.html', libros=libros, existe=True)
+
+    else:
+        return render_template('buscar.html')
+
+
+@app.route("/libro/<isbn>", methods=['GET', 'POST'])
+@login_required
+def libro(isbn):
+    if request.method == 'POST':
+
+        califi = int(request.form.get('rating'))
+        descrip = request.form.get('descrip')
+
+        # Obtener ID del librop
+        id_libro = db.execute(text("SELECT id_book FROM book WHERE isbn = :isbn"),
+                              {"isbn": isbn}).fetchone()
+        id_libro = id_libro[0]
+
+        # Se verifica que el usuario actual solo haya reseñado una vez este libro
+        resenias = db.execute(text("SELECT * FROM review WHERE id_user = :id_user AND id_book = :id_book"),
+                              {"id_user": session['user_id'],
+                               "id_book": id_libro})
+
+        if resenias.rowcount != 0:
+            # print("NO SE PUDO")
+            flash("Solo puedes reseñar una vez", "danger")
+            return redirect("/libro/" + isbn)
+
+        # Si no hay reseñas se inserta en la base de datos
+        db.execute(text("""INSERT INTO review (rate, reviewer, id_user, id_book) VALUES 
+                    (:rate, :reviewer, :id_user, :id_book)"""),
+                   {"rate": califi,
+                    "reviewer": descrip,
+                    "id_user": session['user_id'],
+                    "id_book": id_libro})
+
+        db.commit()
+
+        # print("CLAROOO SE PUDO")
+        flash("Reseña Enviada", "success")
+        return redirect("/libro/" + isbn)
+    else:
+        # Consulta al libro (datos)
+        datosLibro = db.execute(
+            text("SELECT isbn, title, author, year FROM book WHERE isbn=:isbn"), {'isbn': isbn})
+        datosLibro = datosLibro.fetchall()
+
+        # Consulta de API GOOGLE BOOKS como json-----------------
+        libroGR = requests.get(
+            "https://www.googleapis.com/books/v1/volumes?q=isbn:"+isbn).json()
+
+        # accedo en donde esta la informacion que necesito
+        libroGR = libroGR['items']
+        libroGR = libroGR[0]['volumeInfo']
+
+        # Conusulta reseñas de usuario lcoal ------------------
+
+        # Obtener ID del librop
+        id_libro = db.execute(text("SELECT id_book FROM book WHERE isbn = :isbn"),
+                              {"isbn": isbn}).fetchone()
+        id_libro = id_libro[0]
+
+        resenias = db.execute(text(f""" SELECT U.username, reviewer, rate FROM review AS R 
+                                    INNER JOIN users AS U 
+                                    ON U.id=R.id_user WHERE R.id_book={id_libro}""")).fetchall()
+
+        # print(libroGR)
+        # print(resenias[0])
+        return render_template('libro.html', datosLibro=datosLibro, libroGR=libroGR, review=resenias)
+
+
+@app.route('/api/<isbn>', methods=['GET'])
+def api(isbn):
+
+    # Consulta de todos los datos para el JSON
+    consulta = db.execute(text("""
+        SELECT title, author, isbn, year, COUNT(R.id_resenias) AS review_count, AVG(R.calificacion) AS average_score 
+        FROM book AS L
+        INNER JOIN resenias AS R ON L.id_libros=R.id_book
+        WHERE isbn=:isbn
+        GROUP BY title, author, isbn, year
+    """), {'isbn': isbn}).fetchone()
+
+    if consulta is None:
+        return jsonify({'Posibles Errores': {'Error ISBN': 'ISBN ingresada Invalida',
+                                             'Libro': 'Este libro no tiene resenias'}}), 404
+
+    # convertir a dict
+    consulta = dict(consulta)
+
+    # dar formato para mostrar dos decimales
+    consulta['average_score'] = float('%.2f' % (consulta['average_score']))
+
+    return jsonify(consulta)
+
+
+if __name__ == "__main__":
+    app.run()
